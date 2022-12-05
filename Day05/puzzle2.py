87ef84cf434aa8490e6b2ed574be6ee7015a0df9
TigerCZ <tiger.cze@post.cz>
2022-12-05 12:04:59 +0100

    Solved Day05 - Python

---
+++ Day05/puzzle2.py
@@ -0,0 +1,54 @@
+if 0:
+    INPUT_FILE = "input_simple.txt"
+else:
+    INPUT_FILE = "input.txt"
+
+
+moves: list[tuple[int, int, int]] = []
+stacks: list[list[str]] = []
+
+
+def parse_cargo(line: str):
+    stack_idx:int = 0
+    ch_idx:int = 0
+    
+    while ch_idx < len(line):
+        if line[ch_idx] == "[":
+            stacks[stack_idx].append(line[ch_idx + 1]) 
+        
+        ch_idx += 4
+        stack_idx += 1
+    
+
+with open(INPUT_FILE, "r") as file:
+    for line in file.readlines():
+        if line == "\n":
+            continue
+        
+        if line[-1] == '\n':
+            line = line[:-1]
+        
+        if line.startswith("move"):
+            line = line.split(" ")
+            move = (int(line[1]), int(line[3]) - 1, int(line[5]) - 1)
+            moves.append(move)
+            
+        elif line.startswith(" 1"):
+            continue
+        
+        elif line.startswith("[") or line.startswith("    "):
+            if len(stacks) == 0:
+                stacks = [[] for _ in range(len(line)//4 + 1)]
+            
+            parse_cargo(line)
+            
+        else:
+            print(f"Can not parse line: '{line}'")
+
+stacks = [list(reversed(stack)) for stack in stacks]         
+
+for move in moves:
+    stacks[move[2]] += reversed([stacks[move[1]].pop() for _ in range(move[0])])
+    
+print("".join([stack[-1] for stack in stacks]))
+        
